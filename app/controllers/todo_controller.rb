937cf39d72c6598a079938d290268cec63112bd4
Maggie Tang <tangmaggie2002@gmail.com>
2017-06-18 17:04:35 +0000

    Install postgres and update database.yml

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,12 @@
 class TodoController < ApplicationController
     def index
     end
+    def show
+    end
+    def page
+    end
+    def login
+    end
+    def assignments
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
+  get 'todo/show', to: 'todo#show'
+  get 'todo/home', to: 'todo#home'
+  get 'todo/page', to: 'todo#page'
+  get 'todo/login', to: 'todo#login'
+  get 'todo/assignments', to: 'todo#assignments'
 
 end
