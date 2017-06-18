fa2d93b1b048656d993db2efcf78847d18b93491
Maggie Tang <tangmaggie2002@gmail.com>
2017-06-18 20:33:35 +0000

    Define instance variables in Todo show action, and place in show view

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,6 +2,11 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        @assignment_description = "Read Chapter 1"
+        @assignment_due_date = 20170619
+        @assignment_subject = "AP World"
+        @assignment_time_estimte_in_min = 15
+        @assignment_is_complete = true
     end
     def page
     end
